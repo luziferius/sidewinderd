796e836cf4ea4db3f1534e168b82273a8d7b17b2
Thomas Hess <thomas.hess@udo.edu>
2017-04-23 16:12:47 +0200

    Moved almost everything from cli_help.hpp to the source file. This is done to not pollute the namespace of main with internal stuff of the help formatter.
    Signed-off-by: Thomas Hess <thomas.hess@udo.edu>

--- src/core/cli_help.cpp
+++ src/core/cli_help.cpp
@@ -6,10 +6,83 @@
  */
 #include "cli_help.hpp"
 
+#include <string.h>
+
+
+constexpr int HELP_OUTPUT_LINE_WIDTH = 80;
+/**
+ * Minimal distance between a command line switch and its description.
+ */
+constexpr int HELP_DESCRIPTION_TEXT_HORIZONTAL_DISTANCE = 4;
+
+/**
+ * Defines the components of a command line switch help line.
+ * Do not specify dashes for short options (-) or long options (--).
+ * The formatter will add those while processing.
+ * String options default to empty strings instead of undefined values,
+ * so definitions of unneeded elements can be omitted.
+ */
+struct program_option {
+	/** The short option character. Should be a single character string only. */
+	const char* short_option = "";
+	const char* long_option = "";
+	/** The textual option description. Can be of any length. The formatter will do line wrapping. */
+	const char* description = "";
+	const char* argument = "";
+	/**
+	 * leading_newlines can be used to specify how many empty lines are printed before printing
+	 * this element.
+	 */
+	uint leading_newlines = 0;
+};
+
+/** 
+ * Define command line argument help here. Element order defines the print output order.
+ * NOTE: Element initialisation must be in order of struct declaration and not miss
+ * any middle element. Otherwise compilation fails with error message
+ * "sorry, unimplemented: non-trivial designated initializers not supported"
+ * (at least when using g++ 5.4.0).
+ * This compiler restriction reduces the usefulness of this construct a bit, but
+ * may be lifted in the future.
+ */
+constexpr struct program_option available_options[] = {
+	{
+		.short_option = "c",
+		.long_option =  "config",
+		.description =  "use the given configuration file instead of the default",
+		.argument =     "FILE",
+	},
+	{
+		.short_option = "d",
+		.long_option =  "daemon",
+		.description =  "run the program in the background",
+	},
+	{
+		.short_option = "p",
+	},
+	{
+		.short_option = "h",
+		.long_option =  "help",
+		.description =  "display this help and exit",
+		.argument =     "",
+		.leading_newlines = 2,
+	},
+	{
+		.short_option = "v",
+		.long_option =  "version",
+		.description =  "output version information and exit",
+	},
+};
+
+/*
+ * Formatter code below.
+ */
+
+
 /**
  * Calculate the indention width for descriptions.
  * This is the amount of characters in a line before the description starts:
- *   -l --long_option ARG   description of argument
+ *   -l --long_option ARG   description of long_option
  * _________________________ ← indention width
  * 
  * This function is used to align all description texts, even when line wrapping is required.
@@ -82,6 +155,8 @@ const std::string get_formatted_help_text(const std::string program_name) {
 		.append("usage:\t").append(program_name).append(" [OPTION]..." "\n")
 		.append("Enables usage of advanced features of Microsoft Sidewinder X4/X6 keyboards and some" "\n")
 		.append("supported Logitech keyboards (like G105, G710 and G710+)." "\n")
+		.append("This includes macro recording and playback, profile switching and more." "\n")
+		.append("It is meant to be run as a background service or daemon." "\n")
 		.append("\n")
 		.append("Mandatory arguments to long options are mandatory for short options too." "\n")
 		.append(format_option_help())
